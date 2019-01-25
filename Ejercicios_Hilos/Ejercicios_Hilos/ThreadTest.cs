1113c7430130d76066f9cff040d08ffe76614b05
Supercómputo <Supercmputo@LRT-PC>
2019-01-25 10:43:35 -0600

    Ejercicios de supercómputo 1

---
+++ Ejercicios_Hilos/Ejercicios_Hilos/ThreadTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+
+namespace Ejercicios_Hilos
+{
+    public class ThreadTest
+    {
+        bool done;
+        static void Main(string[] args)
+        {
+            // EJERCICIO 2
+            ThreadTest tt = new ThreadTest();       // Crea una instancia en común
+            new Thread(tt.Go).Start();
+            tt.Go();
+            Console.ReadKey();
+            /* 
+            //EJERCICIO 1
+            Thread t = new Thread(WriteY);
+            t.Start();
+
+            for (int i = 0; i < 100000; i++)
+                Console.Write("x");
+
+            Console.ReadKey();
+            */
+        }
+        /// <summary>
+        /// Go es una instancia
+        /// EJERCICIO 2
+        /// </summary>
+        void Go()
+        {
+            if (!done)
+            {
+                done = true;
+                Console.WriteLine("Done :D");
+            }
+        }
+        /// <summary>
+        /// Impresiones de y
+        /// EJERCICIO 1
+        /// </summary>
+        public static void WriteY()
+        {
+            for (int i = 0; i < 100000; i++)
+                Console.Write("y");
+        }
+    }
+}
